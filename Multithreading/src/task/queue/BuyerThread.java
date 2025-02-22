2eb2b9322034976558638c66662d9469071f9af3
Val <v.e.reznikov99@gmail.com>
2025-02-22 17:46:50 +0200

    main: Multithreading NotifyAll

--- Multithreading/src/task/queue/BuyerThread.java
+++ Multithreading/src/task/queue/BuyerThread.java
@@ -11,21 +11,25 @@ public class BuyerThread implements Runnable {
 
     @Override
     public void run() {
-
         try {
             while (true) {
-                if (!cashBoxes.isEmpty()) {
-                    CashBox cashBox = cashBoxes.remove();
-                    System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashBox);
+                synchronized (cashBoxes) {
+                    if (!cashBoxes.isEmpty()) {
+                        CashBox cashBox = cashBoxes.remove();
+                        System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashBox);
 
-                    Thread.sleep(5L);
+//                        Thread.sleep(5L);
+                        cashBoxes.wait(5L);
 
-                    System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashBox);
-                    cashBoxes.add(cashBox);
-                    break;
-                } else {
-                    System.out.println(Thread.currentThread().getName() + " ожидаю свободную кассу ");
-                    Thread.sleep(5L);
+                        System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashBox);
+                        cashBoxes.add(cashBox);
+                        cashBoxes.notifyAll();
+                        break;
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " ожидаю свободную кассу ");
+//                        Thread.sleep(5L);
+                        cashBoxes.wait();
+                    }
                 }
             }
         } catch (InterruptedException e) {
--- Multithreading/src/task/queue/CashBox.java
+++ Multithreading/src/task/queue/CashBox.java
@@ -4,7 +4,6 @@ public class CashBox {
     private static int generator = 1;
     private int id;
 
-
     public CashBox() {
         this.id = generator++;
     }
